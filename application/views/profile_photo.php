ef899b5d3a2f3a2e8283063b1444b0c517fac0c2
vimalring <vimalring@gmail.com>
2013-10-01 19:16:57 +0530

    Image Upload button Script works
    Profile Photo Page Upload button UI changes

--- application/views/profile_photo.php
+++ application/views/profile_photo.php
@@ -4,19 +4,21 @@
 <h4>Profile Photo</h4>
 <!--Hobby box1 start-->
 <div class="row">
-<a href="profile_photo_crop.html">
+<a href="#"  onclick="$('input[id=lefile]').click();">
 <div class="profile_photo_space row-fluid">
 <div class="profile_photo_space_inner"></div>
 </div>
 </a>
 <div class="profile_photo_up">
-
-
-
 <form method="post" enctype="multipart/form-data" action="" class="form-search">
 <input type="hidden" name="checkval" value="yes">
-<input  name="userfile" type="file">
-<input type="submit" value="upload Photo"  class="btn" >
+<div class="profile_photo_up"></div>
+
+
+<input id="lefile" type="file" style="display:none" name="userfile" onchange="this.form.submit();">
+<input id="photoCover" class="input-medium search-query " type="text" onFocus="$('input[id=lefile]').click();">&nbsp;
+<a class="btn btn-success" onclick="$('input[id=lefile]').click();">Browse</a>
+<br>
 </form>
 </div>
 <div class="hobby_name muted">
@@ -31,3 +33,9 @@
 
 <!--Hobby box1 end-->
 </div>
+<script>
+$("#upload_file").change(function () {
+$('#upload_video').submit();
+});
+
+</script>
