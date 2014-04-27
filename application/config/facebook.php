783778f578eaafd519859ec60a4a5c50223821f9
vimalring <vimalring@gmail.com>
2014-04-27 18:26:40 +0530

    test
    test

--- application/config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	
-
-$config['appId'] = '189333361246931';
-$config['secret'] = 'we need to put secret code in our server';
-		
-
